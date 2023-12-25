38d0baa8486f5aa5acda6cc9f114d43bd03fa42c
userhub2020 <73968050+userhub2020@users.noreply.github.com>
2023-12-25 20:07:26 +0100

    Serial Number des Clients ausgeben

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+// const client = getClient(serialNumber);
+// if (!client)
+// return;
+// 
+// client.ping = input.runningTime()
+// client.sprite.setBrightness(Math.max(1, receivedNumber & 0xff));
+radio.onReceivedNumber(function (receivedNumber) {
+    serialNumber = radio.receivedPacket(RadioPacketProperty.SerialNumber)
+    basic.showNumber(serialNumber)
+})
+let serialNumber = 0
 radio.setGroup(4)
 basic.forever(function () {
 	
